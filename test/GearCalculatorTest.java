38ab4f355e7e88b7358323aa667bb7575b9a3e5d
Tomasz Pałucki <tpalucki@gmail.com>
2020-05-25 23:25:58 +0200

    Tests added for gear calculator.

--- test/GearCalculatorTest.java
+++ test/GearCalculatorTest.java
@@ -12,25 +12,18 @@ class GearCalculatorTest {
         return new GearCalculator(defaultGearRange);
     }
 
-    @BeforeEach
-    void prepareTest() {
-
-    }
-
     @Test
-    void calculatePrevious() {
+    void calculatePreviousInEco() {
         // given
         GearCalculator calculator = givenGearCalculator();
-        RPM rpm = RPM.rpm(500);
-        Gear gear = new Gear(2);
-        GasPosition gasPosition = GasPosition.of(0.9d);
-
-        // when - then
-        Assertions.assertEquals(new Gear(1), calculator.calculate(rpm, gear, gasPosition, DriveMode.ECO));
+        // when
+        Gear result = calculator.calculate(RPM.rpm(500), new Gear(2), GasPosition.of(0.9d), DriveMode.ECO);
+        // then
+        Assertions.assertEquals(1, result.asInt());
     }
 
     @Test
-    void calculateNext() {
+    void calculateNextInEco() {
         // given
         GearCalculator calculator = givenGearCalculator();
         RPM rpm = RPM.rpm(2500);
@@ -38,7 +31,7 @@ class GearCalculatorTest {
         GasPosition gasPosition = GasPosition.of(0.9d);
 
         // when - then
-        Assertions.assertEquals(new Gear(3), calculator.calculate(rpm, gear, gasPosition,  DriveMode.ECO));
+        Assertions.assertEquals(new Gear(3), calculator.calculate(rpm, gear, gasPosition, DriveMode.ECO));
     }
 
     @Test
@@ -49,7 +42,88 @@ class GearCalculatorTest {
         GasPosition gasPosition = GasPosition.of(0.9d);
 
         // when - then
-        Assertions.assertEquals(max, calculator.calculate(rpm, max,  gasPosition, DriveMode.ECO));
+        Assertions.assertEquals(max, calculator.calculate(rpm, max, gasPosition, DriveMode.ECO));
+    }
 
+    @Test
+    void whenMaxGearAndRpmAboveOptimalShouldCalculateMaxGear() {
+        // given
+        GearCalculator calculator = givenGearCalculator();
+        // when
+        Gear result = calculator.calculate(RPM.rpm(50), min, GasPosition.of(0.9d), DriveMode.ECO);
+        // then
+        Assertions.assertEquals(min, result);
+    }
+
+    @Test
+    void whenMinialAndRpmBelowOptimalRangeShouldReturnMinimalGear() {
+        // given
+        GearCalculator calculator = givenGearCalculator();
+        // when
+        Gear result = calculator.calculate(RPM.rpm(9000), max, GasPosition.of(0.9d), DriveMode.ECO);
+        // then
+        Assertions.assertEquals(max, result);
+    }
+
+    @Test
+    void noKickdownOnEcoMode() {
+        // given
+        GearCalculator calculator = givenGearCalculator();
+        // when
+        Gear result = calculator.calculate(RPM.rpm(1750), new Gear(5), GasPosition.of(0.1d), DriveMode.ECO);
+        // then
+        Assertions.assertEquals(new Gear(5), result);
+    }
+
+    @Test
+    void kickdownOnComfortMode() {
+        // given
+        GearCalculator calculator = givenGearCalculator();
+        // when
+        Gear result = calculator.calculate(RPM.rpm(2000), new Gear(5), GasPosition.of(0.45d), DriveMode.COMFORT);
+        // then
+        Assertions.assertEquals(4, result.asInt());
+    }
+
+    @Test
+    void noStrongKickdownOnComfortMode() {
+        // given
+        GearCalculator calculator = givenGearCalculator();
+        // when
+        Gear result = calculator.calculate(RPM.rpm(2000), new Gear(5), GasPosition.of(0.1d), DriveMode.COMFORT);
+        // then
+        Assertions.assertEquals(4, result.asInt());
+
+    }
+
+
+    @Test
+    void lightKickdownOnSportMode() {
+        // given
+        GearCalculator calculator = givenGearCalculator();
+        // when
+        Gear result = calculator.calculate(RPM.rpm(2000), new Gear(5), GasPosition.of(0.69d), DriveMode.SPORT);
+        // then
+        Assertions.assertEquals(4, result.asInt());
+    }
+
+    @Test
+    void strongKickdownOnSportMode() {
+        // given
+        GearCalculator calculator = givenGearCalculator();
+        // when
+        Gear result = calculator.calculate(RPM.rpm(2000), new Gear(5), GasPosition.of(0.49d), DriveMode.SPORT);
+        // then
+        Assertions.assertEquals(3, result.asInt());
+    }
+
+    @Test
+    void sportModeStrongKickdownWhenSecondGear() {
+        // given
+        GearCalculator calculator = givenGearCalculator();
+        // when
+        Gear result = calculator.calculate(RPM.rpm(2000), new Gear(2), GasPosition.of(0.1d), DriveMode.SPORT);
+        // then
+        Assertions.assertEquals(1, result.asInt());
     }
 }
