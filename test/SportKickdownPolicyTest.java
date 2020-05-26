cfd33db341962e30e9b90c70d6a52cbb5f18d443
Tomasz Pałucki <tpalucki@gmail.com>
2020-05-26 15:19:05 +0200

    Added Two tests for Sport Kickdown Policy.

--- test/SportKickdownPolicyTest.java
+++ test/SportKickdownPolicyTest.java
@@ -7,6 +7,16 @@ class SportKickdownPolicyTest {
     private KickdownPolicy kickdownPolicy = new SportKickdownPolicy(GasPosition.of(0.5d), RPM.k(5d), GasPosition.of(0.7d), RPM.k(5d));
 
     @Test
+    void shouldNotBeApplicableWhenGasPositionAboveRange() {
+        assertFalse(kickdownPolicy.isApplicable(GasPosition.of(0.8d)));
+    }
+
+    @Test
+    void shouldNotBeApplicableWhenGasPositionEqualsRange() {
+        assertFalse(kickdownPolicy.isApplicable(GasPosition.of(0.7d)));
+    }
+
+    @Test
     void shoudBeApplicableOnStrongKickdown() {
         assertTrue(kickdownPolicy.isApplicable(GasPosition.of(0.1d)));
     }
@@ -16,10 +26,6 @@ class SportKickdownPolicyTest {
         assertTrue(kickdownPolicy.isApplicable(GasPosition.of(0.69d)));
     }
 
-    @Test
-    void shouldNotBeApplicableWhenGasPositionEqualsRange() {
-        assertFalse(kickdownPolicy.isApplicable(GasPosition.of(0.7d)));
-    }
 
     @Test
     void shouldBeApplicableWhenGasPositionEqualsLowerThreshold() {
